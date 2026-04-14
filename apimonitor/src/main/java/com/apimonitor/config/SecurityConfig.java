7559669677fd9dd220782d896abc550e0cb194cd
Raghvendra Yadav <raghvendrayadavsms@gmail.com>
2026-04-14 19:50:04 +0530

    update docker code

--- apimonitor/src/main/java/com/apimonitor/config/SecurityConfig.java
+++ apimonitor/src/main/java/com/apimonitor/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.apimonitor.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -17,13 +20,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
+                .cors(Customizer.withDefaults())
 
+
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/test/**", "/api/users/register", "/api/users/login").permitAll()
                         .requestMatchers("/api/logs/**", "/api/apis/**").permitAll()
+
                         .anyRequest().authenticated()
                 )
+
                 .addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
