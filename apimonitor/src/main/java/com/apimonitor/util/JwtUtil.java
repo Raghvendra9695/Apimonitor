4d4020a87a7c8c03698f77886c0da4c4dac2a962
Raghvendra Yadav <raghvendrayadavsms@gmail.com>
2026-04-16 00:00:10 +0530

    UserController update

--- apimonitor/src/main/java/com/apimonitor/util/JwtUtil.java
+++ apimonitor/src/main/java/com/apimonitor/util/JwtUtil.java
@@ -1,33 +1,42 @@
 package com.apimonitor.util;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
-
 import java.security.Key;
 import java.util.Date;
-
+import java.nio.charset.StandardCharsets;
 
 public class JwtUtil {
 
-    private static final String SECRET = "mysecretkeymysecretkeymysecretkey";
 
-    private static final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+    private static final String SECRET = "mysecretkeymysecretkeymysecretkey123456";
+
+    private static final Key key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
 
     public static String generateToken(String email) {
-        return Jwts.builder()
-                .setSubject(email)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(key)
-                .compact();
+        try {
+            return Jwts.builder()
+                    .setSubject(email)
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                    .signWith(key, SignatureAlgorithm.HS256)
+                    .compact();
+        } catch (Exception e) {
+            System.out.println("❌ Token Generation Error: " + e.getMessage());
+            return null;
+        }
     }
 
     public static String validateToken(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+        } catch (Exception e) {
+            System.out.println("❌ Token Validation Error: " + e.getMessage());
+            return null;
+        }
     }
 }
-
