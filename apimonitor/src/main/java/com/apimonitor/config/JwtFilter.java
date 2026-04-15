3b6be4619bb076c2f90ed0604b2d7290b3954078
Raghvendra Yadav <raghvendrayadavsms@gmail.com>
2026-04-15 20:48:55 +0530

    UserController update

--- apimonitor/src/main/java/com/apimonitor/config/JwtFilter.java
+++ apimonitor/src/main/java/com/apimonitor/config/JwtFilter.java
@@ -3,9 +3,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 import com.apimonitor.util.JwtUtil;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class JwtFilter extends OncePerRequestFilter {
 
@@ -15,8 +18,15 @@ public class JwtFilter extends OncePerRequestFilter {
                                     FilterChain filterChain)
             throws ServletException, IOException {
 
-        String header = request.getHeader("Authorization");
+        String path = request.getServletPath();
 
+
+        if (path.contains("/api/users/login") || path.contains("/api/users/register")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String header = request.getHeader("Authorization");
         if (header == null || !header.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
@@ -26,8 +36,14 @@ public class JwtFilter extends OncePerRequestFilter {
 
         try {
             String email = JwtUtil.validateToken(token);
-            System.out.println("✅ Valid user: " + email);
+            if (email != null) {
 
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(email, null, new ArrayList<>());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                System.out.println("✅ User Authenticated: " + email);
+            }
         } catch (Exception e) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.getWriter().write("Invalid Token ❌");
