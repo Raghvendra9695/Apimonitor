5503a9aa57a22a39c6bf2109db31e0c8c39e83b4
Raghvendra Yadav <raghvendrayadavsms@gmail.com>
2026-04-17 13:47:32 +0530

    UserController update

--- apimonitor/src/main/java/com/apimonitor/service/UserService.java
+++ apimonitor/src/main/java/com/apimonitor/service/UserService.java
@@ -1,5 +1,4 @@
 package com.apimonitor.service;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -30,26 +29,13 @@ public class UserService {
     /**
      * Login logic with Debugging logs
      */
-    public User loginUser(String email, String rawPassword) {
 
+    public User loginUser(String email, String password) {
         User user = userRepository.findByEmail(email);
 
-        if (user != null) {
-
-            boolean isMatch = passwordEncoder.matches(rawPassword, user.getPassword());
-
-
-            System.out.println("DEBUG: Login attempt for email: " + email);
-            System.out.println("DEBUG: Password Match Result: " + isMatch);
-
-            if (isMatch) {
-                return user;
-            }
-        } else {
-            System.out.println("DEBUG: No user found in DB with email: " + email);
+        if (user != null && password.equals(user.getPassword())) {
+            return user;
         }
-
-
         throw new RuntimeException("Invalid credentials");
     }
 }
