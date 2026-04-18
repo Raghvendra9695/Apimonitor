ea9d6704089508c38f73ff9b653c00b3e987be55
Raghvendra Yadav <raghvendrayadavsms@gmail.com>
2026-04-18 19:11:55 +0530

    UserController update

--- apimonitor/src/main/java/com/apimonitor/config/WebConfig.java
+++ apimonitor/src/main/java/com/apimonitor/config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://api-monitor.netlify.app")
+                .allowedOriginPatterns(
+                        "http://localhost:5173",
+                        "https://*.netlify.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
